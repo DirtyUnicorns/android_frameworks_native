8f76d32e3f1eab679bdab03ecc0a0c710c199737
Christopher N. Hesse <raymanfx@gmail.com>
2014-12-05 21:38:51 +0100

    inputflinger: disable touch input while using a stylus
    Android 5.0 cannot form multitouch events from multiple input
    devices. So it is not possible to report one touchpoint from the
    stylus position and, at the same time, another touchpoint from a
    finger touch. Instead, when a new input device starts up the currently
    active input is cancelled. This is highly undesirable while writing
    with the pen.
    
    The easiest solution is to ignore non-stylus touch events while the
    stylus is within range (hovering) of the touchscreen. For example,
    N-trig digitizers implement this in hardware. But wacom digitizers do
    report pen data simultaneously with touch data. This patch disables
    (non-stylus) touch input within 50ms of stylus data. On my Galaxy
    Note this is necessary to make stylus input usable.
    
    Original commit by vbraun:
    https://github.com/CyanogenMod/android_frameworks_base/commit/b9cb2961309c829586ae24ce432bb1fdb5610ba6
    
    Change-Id: I97f26369826e96c97461c8ae188f1c64dec1b4d3

--- services/inputflinger/InputReader.h
+++ services/inputflinger/InputReader.h
@@ -236,6 +236,9 @@ struct InputReaderConfiguration {
 
     // True to show the location of touches on the touch screen as spots.
     bool showTouches;
+    
+    // Ignore finger touches this long after the stylus has been used (including hover)
+    nsecs_t stylusPalmRejectionTime;
 
     // Remap volume keys according to display rotation
     // 0 - disabled, 1 - phone or hybrid rotation mode, 2 - tablet rotation mode
@@ -259,6 +262,7 @@ struct InputReaderConfiguration {
             pointerGestureZoomSpeedRatio(0.3f),
             stylusPalmRejectionTime(50 * 10000000LL), // 50 ms
             showTouches(false),
+            stylusPalmRejectionTime(50 * 10000000LL), // 50 ms
             volumeKeysRotationMode(0) { }
 
     bool getDisplayInfo(bool external, DisplayViewport* outViewport) const;
