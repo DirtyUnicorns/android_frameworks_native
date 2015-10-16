7758aac35e5557352b69d49ba77e80d7908ddc0b
Tomohiro Arikado <tomohiro.arikado@sonymobile.com>
2015-10-16 18:42:50 +0900

    SurfaceFlinger: Remove size check in captureScreen
    Display may have larger physical resolution than DisplayDevice.
    To enable screen capture in physical resolution, remove size check.
    
    Change-Id: Ie6ceff0fd2852e51981da370b7b77969778b0979

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -3348,12 +3348,6 @@ status_t SurfaceFlinger::captureScreenImplLocked(
         std::swap(hw_w, hw_h);
     }
 
-    if ((reqWidth > hw_w) || (reqHeight > hw_h)) {
-        ALOGE("size mismatch (%d, %d) > (%d, %d)",
-                reqWidth, reqHeight, hw_w, hw_h);
-        return BAD_VALUE;
-    }
-
     reqWidth  = (!reqWidth)  ? hw_w : reqWidth;
     reqHeight = (!reqHeight) ? hw_h : reqHeight;
 
