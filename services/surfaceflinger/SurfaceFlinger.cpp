8c2cf9ee978fc08e904674f400164adc348fa78a
Jake Weinstein <xboxlover360@gmail.com>
2016-11-04 12:31:20 -0400

    native: use realtime scheduling for UX-critical items
    * Surfaceflinger and Inputflinger have a large impact
      on overall smoothness, so schedule them using the
      Realtime scheduling scheme for optimal user experience.
    
    Change-Id: I6d325e90398d56bfee164af9fa8897b0e3a8a271
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -501,7 +501,7 @@ void SurfaceFlinger::init() {
     getDefaultDisplayDevice()->makeCurrent(mEGLDisplay, mEGLContext);
 
     mEventControlThread = new EventControlThread(this);
-    mEventControlThread->run("EventControl", PRIORITY_URGENT_DISPLAY);
+    mEventControlThread->run("EventControl", PRIORITY_REALTIME);
     android_set_rt_ioprio(mEventControlThread->getTid(), 1);
 
     // initialize our drawing state
